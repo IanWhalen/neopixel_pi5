78584dd15befc6fe701bacf0a9fd43a82f9db014
Ian Whalen <ian.whalen@gmail.com>
2025-09-19 14:14:20 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,7 @@ import asyncio
 
 from viam.module.module import Module
 
-try:
-    from models.neopixel_pi5 import NeopixelPi5  # noqa: F401
-except ModuleNotFoundError:
-    # when running as local module with run.sh
-    from .models.neopixel_pi5 import NeopixelPi5  # type: ignore #noqa: F401
-
+from models.neopixel_pi5 import NeopixelPi5  # noqa: F401
 
 if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
